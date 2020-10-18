d4f1f8341b7eadf6a69135107303d8e74ea42a05
Mateus Fugita <mateushiwao@gmail.com>
2020-10-18 20:10:22 -0300

    show country and images in the result page

--- src/pages/ChosenCountry/index.js
+++ src/pages/ChosenCountry/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import './styles.css';
+
+import Header from '../../components/Header';
+import PhotoSlider from '../../components/PhotoSlider';
 
 export default function ChosenCountry(){
+    const history = useHistory();
+    const [country, setCountry] = useState('');
+    const [images, setImages] = useState([]);
+    const [ptName, setPtName] = useState('');
+
+    useEffect(() => {
+        if(!history.location.data){
+            history.push('/');
+        }
+        else{
+            setCountry(history.location.data.country);
+            setImages(history.location.data.images);
+            setPtName(history.location.data.ptName);
+        }
+    }, [history]);
+
     return (
-        <h1>País</h1>
+        <>
+            <Header />
+            <main className='results-container'>
+                <div className='result-text'>
+                    <h3>Seu destino escolhido: {ptName}</h3>
+                </div>
+                <PhotoSlider images={images}/>
+            </main>
+        </>
     )
 }
