efa5876876f3b92498d023fcd03c0d5040f9c363
Mateus Fugita <mateushiwao@gmail.com>
2020-10-18 20:19:55 -0300

    get response from API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import './styles.css';
 
+import Header from '../../components/Header';
 import Question from '../../components/Question';
+import api from '../../services/api';
 
 export default function Home(){
-    const questions = ['Pergunta 1', 'Pergunta 2'];
+    const questions = ['Pergunta 1', 'Pergunta 2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
     const history = useHistory();
+    let answers = Array(questions.length);
+    const [token, setToken] = useState('');
 
-    function handleSubmit(){
-        history.push('/result');
+    useEffect(() => {
+        const data = {
+            username: process.env.REACT_APP_USERNAME,
+            password: process.env.REACT_APP_PASSWORD
+        };
+
+        api.post('user/authenticate', data).then(response => {
+            setToken(response.data.token);
+        });
+    }, []);
+
+    async function handleSubmit(){
+        if(!answers.includes(undefined)){
+            let data = {};
+
+            data.country = await api.post('country/predict', { answers }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then(async (response) => {
+                const countryNames = await handleRequest('GET', `country?name=${response.data.country}`, null);
+                data.ptName = countryNames.country.ptName;
+                data.abbreviation = countryNames.country.abbreviation;
+                return response.data.country;
+            });
+
+            data.images = await handleRequest('GET', `country/images/${data.country}`, null);
+
+            history.push({
+                pathname: '/result',
+                data: data,
+            });
+        }
+        else{
+            alert('Responda todas as perguntas para continuar');
+        }
+    }
+
+    async function handleRequest(method, url, data){
+        const responseFromAPI = await api({
+            method,
+            url,
+            data,
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(response => {
+            return response.data;
+        });
+        return responseFromAPI;
     }
 
     function setAnswerForQuestion(e){
-        console.log(e.target.value);
-        console.log(e.target.name);
+        answers[e.target.name.substring(1)] = e.target.value;
+
+        const buttonsFromSelectedQuestion = document.getElementsByName(e.target.name);
+        buttonsFromSelectedQuestion.forEach(btn => {
+            if(btn.className.includes(' active')){
+                btn.className = btn.className.replace(' active', '');
+            }
+        })
+
+        e.target.className += ' active';
     }
 
     return (
         <>
-            <header>
-                <h1 className='title'>Check-IA</h1>
+            <Header />
+            <main className='questions-container'>
                 <p className='subtitle'>Descubra qual será seu próximo destino de acordo com o seu perfil</p>
-            </header>
-            <main>
                 {questions.map((question, index) => {
                     return (
-                        <Question question={question} index={index} handleClickFunction={setAnswerForQuestion}></Question>
+                        <Question key={index} question={question} index={index} handleClickFunction={setAnswerForQuestion}></Question>
                     )
                 })}
                 <button className='send-answers' onClick={handleSubmit}>Enviar</button>
