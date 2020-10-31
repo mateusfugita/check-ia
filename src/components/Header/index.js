65c83f4623a6e678930b1d23141a4f9582f3794a
Mateus Fugita <mateushiwao@gmail.com>
2020-10-31 17:02:17 -0300

    change header layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,12 @@ import './styles.css';
 export default function Header(){
     return (
         <header>
+            <div className='header-opaque'>
                 <h1 className='title'>Check-IA</h1>
+                <p className='subtitle'>Descubra qual será seu próximo destino de acordo com o seu perfil</p>
+            </div>
+            {/* <h1 className='title'>Check-IA</h1>
+            <p className='subtitle'>Descubra qual será seu próximo destino de acordo com o seu perfil</p> */}
         </header>
     )
 }
